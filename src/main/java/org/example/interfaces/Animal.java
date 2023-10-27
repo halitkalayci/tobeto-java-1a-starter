4acb24b129aab00244e7a1f902d674618367cde0
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-10-27 10:35:56 +0300

    interfaces

---
+++ src/main/java/org/example/interfaces/Animal.java
@@ -0,0 +1,9 @@
+package org.example.interfaces;
+
+public interface Animal {
+    void animalSound();
+
+    void breathe();
+
+    void eat();
+}
---
+++ src/main/java/org/example/interfaces/Dog.java
@@ -0,0 +1,21 @@
+package org.example.interfaces;
+
+// Extends => Class
+// Implements => Interface
+public class Dog implements Animal
+{
+    @Override
+    public void animalSound() {
+
+    }
+
+    @Override
+    public void breathe() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+}
